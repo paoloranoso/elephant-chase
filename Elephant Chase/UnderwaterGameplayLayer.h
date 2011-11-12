d1d5a09924ccb6fdc9cfafb422df650d4da6887d
Paolo Ranoso <paoloranoso@gmail.com>
2011-11-12 05:05:21 -0500

    move hero left or right

--- Elephant Chase/UnderwaterGameplayLayer.h
+++ Elephant Chase/UnderwaterGameplayLayer.h
@@ -8,11 +8,18 @@
 
 #import "cocos2d.h"
 
+#import "SneakyJoystick.h" //need joystick one for delegate methods
+#import "SneakyButton.h"
+#import "SneakyButtonSkinnedBase.h"
+
 @interface UnderwaterGameplayLayer : CCLayer{
     CCSprite *hero;
     CCSprite *elephant;
     CCSprite *boat;
     CCSprite *bomb;    
+    
+    SneakyButton *leftButton; 
+    SneakyButton *rightButton;    
     
     CGSize screenSize;
 }
--- Elephant Chase/UnderwaterGameplayLayer.m
+++ Elephant Chase/UnderwaterGameplayLayer.m
@@ -9,6 +9,13 @@
 
 #import "UnderwaterGameplayLayer.h"
 
+#define kDirectionalButtonLength 64.0f
+
+
+@interface UnderwaterGameplayLayer(PrivateMethods)
+-(void)initDirectionalButtons;
+@end
+
 @implementation UnderwaterGameplayLayer
 
 
@@ -23,14 +30,80 @@
         [self addChild:hero];
         
         
+        //TODO: more sprite adding here for elephant, boat, bomb
+    
         
-//        CCSprite *backgroundImage = [CCSprite spriteWithFile:@"underwater-background.png"];
-//        
-//        [backgroundImage setPosition: ccp(screenSize.width/2, screenSize.height/2)];
-//        [self addChild:backgroundImage z:0 tag:0];        
+        [self initDirectionalButtons];
+        
+        [self scheduleUpdate];
+        
 	}
 	return self;
 }
+
+
+-(void)initDirectionalButtons{
+    CGRect leftButtonDimensions = CGRectMake(0, 0, kDirectionalButtonLength, kDirectionalButtonLength);
+    CGRect rightButtonDimensions = CGRectMake(0, 0, kDirectionalButtonLength, kDirectionalButtonLength);    
+    CGPoint leftButtonPosition = ccp(screenSize.width*0.2f, screenSize.height*0.2f);
+    CGPoint rightButtonPosition = ccp(screenSize.width*0.8f, screenSize.height*0.2f);    
+    
+    SneakyButtonSkinnedBase *leftButtonBase =
+    [[[SneakyButtonSkinnedBase alloc] init] autorelease];
+    leftButtonBase.position = leftButtonPosition;
+    leftButtonBase.defaultSprite =
+    [CCSprite spriteWithFile:@"left-up.png"];
+    leftButtonBase.activatedSprite =
+    [CCSprite spriteWithFile:@"left-down.png"];
+    leftButtonBase.pressSprite =
+    [CCSprite spriteWithFile:@"left-down.png"];
+    leftButtonBase.button = [[SneakyButton alloc]
+                             initWithRect:leftButtonDimensions];
+    leftButton = [leftButtonBase.button retain];
+    leftButton.isToggleable = NO;
+    leftButton.isHoldable = YES;
+    [self addChild:leftButtonBase];
+    
+    
+    SneakyButtonSkinnedBase *rightButtonBase =
+    [[[SneakyButtonSkinnedBase alloc] init] autorelease];
+    rightButtonBase.position = rightButtonPosition;
+    rightButtonBase.defaultSprite = [CCSprite
+                                      spriteWithFile:@"right-up.png"];
+    rightButtonBase.activatedSprite = [CCSprite
+                                        spriteWithFile:@"right-down.png"];
+    rightButtonBase.pressSprite = [CCSprite
+                                    spriteWithFile:@"right-down.png"];
+    rightButtonBase.button = [[SneakyButton alloc]
+                               initWithRect:rightButtonDimensions];
+    rightButton = [rightButtonBase.button retain];
+    rightButton.isToggleable = NO;
+    rightButton.isHoldable = YES;
+    [self addChild:rightButtonBase];    
+}
+
+
+
+#pragma mark -
+#pragma mark Update Method
+-(void) update:(ccTime)deltaTime
+{    
+    
+    //move hero left or right
+    if (leftButton.active == YES) {
+        hero.position = ccp( hero.position.x - 150*deltaTime, hero.position.y);
+    }
+    if (rightButton.active == YES) {
+        hero.position = ccp( hero.position.x + 150*deltaTime, hero.position.y);
+    }
+    
+}
+
+
+
+
+
+
 
 
 - (void) dealloc{
